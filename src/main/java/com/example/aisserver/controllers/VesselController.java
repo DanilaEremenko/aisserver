99cbe55193ec270b0abb52ac24641e22402ba1f3
Danila <danilaeremenko@mail.ru>
2021-12-17 14:55:33 +0300

    aisserver: CrossOrigin annotation added to VesselController

--- src/main/java/com/example/aisserver/controllers/VesselController.java
+++ src/main/java/com/example/aisserver/controllers/VesselController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 class VesselController {
 
